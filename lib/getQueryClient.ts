4e00e3e1c8734efd14fb4bfec466c03690ac4a62
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2026-01-14 15:22:12 +0200

    gerQueryClient.ts

--- lib/getQueryClient.ts
+++ lib/getQueryClient.ts
@@ -1,13 +1,18 @@
 import { QueryClient } from "@tanstack/react-query";
 
+let client: QueryClient | null = null;
+
 export default function getQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5, 
-        gcTime: 1000 * 60 * 10, 
-        refetchOnWindowFocus: false,
+  if (!client) {
+    client = new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 5, // 5 хвилин
+          refetchOnWindowFocus: false,
+        },
       },
-    },
-  });
+    });
+  }
+
+  return client;
 }
