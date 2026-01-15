ebc28700381df89610d9253e5b21083f7094b80c
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2026-01-15 15:40:38 +0200

    fix build errors (css + tanstack provider)

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,39 +1,16 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
-import {
-  QueryClient,
-  QueryClientProvider,
-  Hydrate,
-  DehydratedState,
-} from "@tanstack/react-query";
 
 interface TanStackProviderProps {
   children: ReactNode;
-  dehydratedState?: DehydratedState;
 }
 
-export default function TanStackProvider({
-  children,
-  dehydratedState,
-}: TanStackProviderProps) {
-  // QueryClient створюється один раз у клієнті
-  const [client] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60 * 5,
-            cacheTime: 1000 * 60 * 10,
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
+export default function TanStackProvider({ children }: TanStackProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={client}>
-      <Hydrate state={dehydratedState}>{children}</Hydrate>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
