78229de49bd113d483abb253f159cef8701dc450
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2026-01-15 16:03:27 +0200

    Correct getQueryClient

--- lib/getQueryClient.ts
+++ lib/getQueryClient.ts
@@ -1,23 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-let client: QueryClient | null = null;
-
-export default function getQueryClient() {
-  if (!client) {
-    client = new QueryClient({
-      defaultOptions: {
-        queries: {
-          // 5 хвилин дані вважаються свіжими
-          staleTime: 1000 * 60 * 5,
-
-          // час життя кешу (react-query v4 використовує cacheTime)
-          cacheTime: 1000 * 60 * 10,
-
-          refetchOnWindowFocus: false,
-        },
+export function getQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        gcTime: 1000 * 60 * 10,
+        refetchOnWindowFocus: false,
       },
-    });
-  }
-
-  return client;
+    },
+  });
 }
